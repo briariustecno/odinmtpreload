ad06cc506adaf48a7d24d26db036085e181651db
briariustecno <briariustecno@gmail.com>
2020-04-22 10:35:07 -0300

    Reformulando métodos

--- js/control.js
+++ js/control.js
@@ -1,84 +1,30 @@
 document.getElementById('btn-submit').addEventListener('click', redefineSubmit, false);
 
-function redefineUpload(evt) {
-    evt.preventDefault();
-    getData();
-}
-
-function redefineExcel(evt) {
-    evt.preventDefault();
-    getExcel();
-}
-
 function redefineSubmit(evt) {
     evt.preventDefault();
     const data = {
         user : document.getElementById('first').value,
         adress : document.getElementById('second').value,
-        idgroup : document.getElementById('third').value,
-        qtdfloors : document.getElementById('fourth').value,
-        qtdapto : document.getElementById('fifth').value,
-        qtdhouse : document.getElementById('sixth').value,
+        id : document.getElementById('third').value,
+        andares : document.getElementById('fourth').value,
+        aptos : document.getElementById('fifth').value,
+        casas : document.getElementById('sixth').value,
         obs : document.getElementById('seventh').value
     }
-    let cop = MakeLine(data);
-    checkForm(cop, data.adress);
+
+    checkForm(JSON.stringify(data));
 }
 
-function MakeLine ( msg ) {
-    var line = 'idpredio' + ';' + 'andar'+ ';' + 'apartamento' + ';' + 'casa;\r\n';
-    const id = msg.idgroup;
-    let floors = parseInt(msg.qtdfloors);
-    let apto = parseInt(msg.qtdapto);
-    for (i=0;i<floors;i++) {
-        var flo = i+1;
-        for (j=1;j<=apto;j++) {
-            let fib = flo /* 100*/;
-            let apt = j + fib;
-            line += id + ";" + /*flo +*/ ";" + apt + ";;\r\n";
-        }
-    }
-    return line;
-}
-
-function checkForm ( body, adr ) {
-    let cfg = new Object;
-    cfg.one = body;
-    cfg.two = adr;
+function checkForm (sdata) {
+    // console.log(sdata);
     var xhttp = new window.XMLHttpRequest();
     xhttp.open('POST', '/form_search', true); 
-    xhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xhttp.setRequestHeader('Content-Type', 'application/json');
     xhttp.onreadystatechange = function() {
         if ( xhttp.readyState == 4 && xhttp.status == 200 ) {
-            var respost = JSON.parse(xhttp.responseText);
-            console.log(respost[0]);
+            var respost = xhttp.responseText;
+            console.log(respost);
         }
     }
-    xhttp.send(JSON.stringify(cfg));
-}
-
-function getData() {
-    var xhttp = new window.XMLHttpRequest();
-    xhttp.open('GET', '/form_upload', true);
-    xhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-    xhttp.onreadystatechange = function() {
-        if ( xhttp.readyState == 4 && xhttp.status == 200 ) {
-            var respost = JSON.parse(xhttp.responseText);
-            console.log("ok");
-        }
-    }
-    xhttp.send();
-}
-
-function getExcel() {
-    var xhttp = new window.XMLHttpRequest();
-    xhttp.open('GET', '/form_excel', true);
-    xhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-    xhttp.onreadystatechange = function() {
-        if ( xhttp.readyState == 4 && xhttp.status == 200 ) {
-            var respost = JSON.parse(xhttp.responseText);
-            console.log("ok");
-        }
-    }
-    xhttp.send();
+    xhttp.send(sdata);
 }
--- server.js
+++ server.js
@@ -15,13 +15,16 @@ app.get('/', function(req, res){
 app.use(bodyParser.json())
 
 app.post('/form_search', function (req, res) {
-    let date = req.body;
-    console.log(date.one);
+    // let date = archger(req);
+    console.log(req.body);
+    // res.send(JSON.stringify(req.body))
 
-    let name = date.two;
-    let hpath =  SetNamePath(name);
-    let path = 'C:\\Odin\\' + hpath;
-    console.log(path);
+    new Promise((res, rej) => {
+        var result = archger(req.body);
+        res(result)
+    }).then((data) => {
+        res.send(data);
+    })
     if (!fs.existsSync(dir)){
         //Efetua a criação do diretório
         fs.mkdir(dir, (err) => {
@@ -33,109 +36,35 @@ app.post('/form_search', function (req, res) {
             console.log("Diretório criado!")
         });
     }
-    fs.writeFile(path, date.one,{enconding:'utf-8',flag: 'a'}, function(erro) {
+    fs.writeFile(path, "teste",{enconding:'utf-8',flag: 'a'}, function(erro) {
 
         if(erro) {
             console.log(erro)
         } else {
             console.log("Arquivo salvo!");
+            res(date);
         }        
     }); 
 })
 
-app.get('/form_upload', function(req, res) {
-    console.log("Heyman");
-    const teste = fs.readFileSync("C:\\Odin\\geodata.json" , "utf8");
-    const convert = JSON.parse(teste);
-    const origin = objToArr(convert);
-    // console.log(origin[0]);
-    // var convertida = original.map(function(obj) {
-    //     return Object.keys(obj).map(function(chave) {
-    //         return obj[chave];
-    //     });
-    // });
-})
-
-app.get('/form_excel', function(req, res) {
-    console.log("Heyman");
-    const SheetOne = fs.readFileSync("C:\\Odin\\Plan1.csv" , "utf8");
-    const SheetTwo = fs.readFileSync("C:\\Odin\\Plan2.csv" , "utf8");
-    console.log(SheetTwo);
-    // console.log(origin[0]);
-    // var convertida = original.map(function(obj) {
-    //     return Object.keys(obj).map(function(chave) {
-    //         return obj[chave];
-    //     });
-    // });
-})
-
-function SetNamePath ( vpath ) {
-    for (i=0;i<=vpath.length;i++) {
-        vpath = vpath.replace(' ', '_')
-        vpath = vpath.replace('.', '')
-        vpath = vpath.replace(',', '')
-        vpath = vpath.replace('ã', 'a')
-        vpath = vpath.replace('Ã', 'A')
-        console.log(vpath);
-    }
-    return vpath + '.txt';
-}
-
-function objToArr (obj) {
-    const vetor = [];
-    const path = 'C:\\Odin\\Gisplay_Audit.csv'
-    for (key of Object.keys(obj)) {
-        vetor.push([key, obj[key]])
-    }
-    
-    var cons = "ID;X;Y;Data;Lat;Long;Tipo_de_Levantamento;" + 
-    "Tipo_de_Logradouro;Tipo_de_Comercial;Nome_do_Condominio;Nome_Estab;" + 
-    "Numero_Logradouro;Qtd_Blocos;Qtd_Andares;Qtd_Apto_Andar;" +
-    "Qtd_Salas;HP_Total;Atendimento;" + "CSC;Alt_Esf;Tensão;Trafo;Travessia;HP;" +
-    "Rua;Lado;Obs.;Node;\r\n";
-    console.log(cons);
-    for(i=0;i<vetor.length;i++) {
-        let object = vetor[i];
-        let key = object[0];
-        let info = object[1];
-        let id = i+1;
-        let x = info.longitude, y = info.latitude;
-        let tlevant = info.type, tlogr = info.typelogr;
-        if (tlevant == "postes") {
-            tlevant = "Poste"
-        } else if (tlevant == "logradouros") {
-            tlevant = "Logradouro";
+function archger(options) {
+    var line = 'idpredio' + ';' + 'andar'+ ';' + 'apartamento' + ';' + 'casa;\r\n';
+    const id = options.id;
+    let floors = parseInt(options.andares);
+    let apto = parseInt(options.aptos);
+    for (i=0;i<floors;i++) {
+        var flo = i+1;
+        for (j=1;j<=apto;j++) {
+            let fib = flo * 100;
+            let apt = j + fib;
+            line += id + ";" + ";" + apt + ";;\r\n";
         }
-        if(tlevant == "" && tlogr == "Unidade(casa)") {
-            tlevant = "Logradouro";
-        }
-        let first = id + ";" + x + ";" + y + ";;" + y + ";" + x + ";" + tlevant + ";" + tlogr + ";";
-        let tcom = info.typecom, ncond = info.namecond, nestb = info.nameestb;
-        let numl = info.number, qtdbl = info.qtdbl, qtdfl = info.qtdfl, apand = info.aptoand;
-        let qtdsl = info.qtdsl, hp = "", atend = info.atend;
-        let csc = info.csc, altesf = info.altesf, tensao = info.tensao;
-        let trafo = info.trafo, trav = info.trav, chp = info.hp, rua = info.street;
-        let lado = info.side, obs = info.obs, node = info.node;
-        let second = tcom + ";" + ncond + ";" + nestb + ";" + numl + ";";
-        let third = qtdbl + ";" + qtdfl + ";" + apand + ";" + qtdsl + ";";
-        let fourth = hp + ";" + atend + ";" + csc + ";" + altesf + ";" + tensao + ";";
-        let fifth = trafo + ";" + trav + ";" + chp + ";" + rua + ";" + lado + ";";
-        let sixth = obs + ";" + node + ";";
-        cons +=  first + second + third + fourth + fifth + sixth+ "\r\n";
     }
-    console.log(cons)
-    fs.writeFile(path, cons,{enconding:'utf-8',flag: 'a'}, function(erro) {
-
-        if(erro) {
-            console.log(erro)
-        } else {
-            console.log("Arquivo salvo!");
-        }        
-    }); 
-}
-
-function LoopSolution() {
-
+    var status = {
+        data: line,
+        status: 'acabou'
+    }
+    return status;
 }
 
 server.listen(port, function () {
